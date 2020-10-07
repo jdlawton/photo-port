daadd290fd06fc2f3640a7cc35058a4acc29c486
JoeL <jlawton1203@gmail.com>
2020-10-07 09:23:44 -0500

    ContactForm component now updates component state with data entered into the form and also created a submit function for future backend development.

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React, {useState} from 'react';
 import Nav from './components/Nav';
 import About from './components/About';
 import Gallery from './components/Gallery';
+import ContactForm from './components/ContactForm';
 
 function App() {
   const [categories] = useState([
@@ -25,12 +26,11 @@ function App() {
         currentCategory={currentCategory}>
       </Nav>
       <main>
-        <div>
+          <ContactForm></ContactForm>
           <Gallery currentCategory={currentCategory}>
 
           </Gallery>
           <About></About>
-        </div>
       </main>
     </div>
   );
